6b5dda4f979f1fd6ace50780dd77fca891afba54
Zhou <zhouyuli@amazon.com>
2017-08-06 16:26:42 -0700

    Show Fav Programs and Saved search in User Account

--- wp-content/themes/pihc/inc/template-functions.php
+++ wp-content/themes/pihc/inc/template-functions.php
@@ -30,3 +30,151 @@ function pihc_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'pihc_pingback_header' );
+
+add_filter('um_account_page_default_tabs_hook', 'favorite_in_um', 100 );
+function favorite_in_um( $tabs ) {
+	$tabs[800]['favorite']['icon'] = 'um-faicon-star';
+	$tabs[800]['favorite']['title'] = 'Favorite Programs';
+	$tabs[800]['favorite']['custom'] = true;
+	return $tabs;
+}
+
+/* make our new tab hookable */
+
+add_action('um_account_tab__favorite', 'um_account_tab__favorite');
+function um_account_tab__favorite( $info ) {
+	global $ultimatemember;
+	extract( $info );
+
+	$output = $ultimatemember->account->get_tab_output('favorite');
+	if ( $output ) { echo $output; }
+}
+
+/* Finally we add some content in the tab */
+
+add_filter('um_account_content_hook_favorite', 'um_account_content_hook_favorite');
+function um_account_content_hook_favorite( $output ){
+	ob_start();
+	?>
+
+	<div class="um-field">
+
+		<!-- Here goes your custom content -->
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th class="manage-column column-primary">
+                        Program Name
+                    </th>
+                    <th class="manage-column column-primary">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                global $post;
+                $args = array(
+                    'post_type'        => 'favorite_program',
+                    'post_status'      => 'publish',
+                    'author'           => get_current_user_id()
+                );
+                $myposts = get_posts( $args );
+
+                foreach ( $myposts as $post ) :
+                  setup_postdata( $post );
+                  $url = $post->post_excerpt;?>
+                    <tr>
+                        <td>
+                            <a href="<?php  echo $url; ?>"><?php the_title(); ?></a>
+                        </td>
+                        <td>
+                            <a href="<?php echo get_delete_post_link($post->ID); ?>">delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach;
+                wp_reset_postdata(); ?>
+            </tbody>
+        </table>
+	</div>
+
+	<?php
+
+	$output .= ob_get_contents();
+	ob_end_clean();
+	return $output;
+}
+
+add_filter('um_account_page_default_tabs_hook', 'saved_search_in_um', 100 );
+function saved_search_in_um( $tabs ) {
+	$tabs[800]['saved_search']['icon'] = 'um-faicon-heart';
+	$tabs[800]['saved_search']['title'] = 'Saved Search';
+	$tabs[800]['saved_search']['custom'] = true;
+	return $tabs;
+}
+
+/* make our new tab hookable */
+
+add_action('um_account_tab__saved_search', 'um_account_tab__saved_search');
+function um_account_tab__saved_search( $info ) {
+	global $ultimatemember;
+	extract( $info );
+
+	$output = $ultimatemember->account->get_tab_output('saved_search');
+	if ( $output ) { echo $output; }
+}
+
+/* Finally we add some content in the tab */
+
+add_filter('um_account_content_hook_saved_search', 'um_account_content_hook_saved_search');
+function um_account_content_hook_saved_search( $output ){
+	ob_start();
+	?>
+
+	<div class="um-field">
+
+		<!-- Here goes your custom content -->
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th class="manage-column column-primary">
+                        Search
+                    </th>
+                    <th class="manage-column column-primary">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                global $post;
+                $args = array(
+                    'post_type'        => 'saved_search',
+                    'post_status'      => 'publish',
+                    'author'           => get_current_user_id()
+                );
+                $myposts = get_posts( $args );
+
+                foreach ( $myposts as $post ) :
+                  setup_postdata( $post );
+                  $url = $post->post_excerpt;?>
+                    <tr>
+                        <td>
+                        <a href="<?php  echo $url; ?>"><?php the_title(); ?></a>
+                        </td>
+                        <td>
+                        <a href="<?php echo get_delete_post_link($post->ID); ?>">delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach;
+                wp_reset_postdata(); ?>
+            </tbody>
+        </table>
+	</div>
+
+	<?php
+
+	$output .= ob_get_contents();
+	ob_end_clean();
+	return $output;
+}
